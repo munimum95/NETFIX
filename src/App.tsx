c4143c7cfd4f88dfd9d4f2b8466329fe163b47c6
JEONGHWANMUN <answjdghks95@gmail.com>
2025-05-12 11:07:59 +0900

    chore: 프로젝트 세팅

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-center py-8">Netfix</h1>
+    </div>
+  );
+}
+
+export default App;
