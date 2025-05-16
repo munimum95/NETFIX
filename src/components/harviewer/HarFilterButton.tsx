2e5c5d1ecf1dcc7220f08ae1563e9115c2e4c5d3
JEONGHWANMUN <answjdghks95@gmail.com>
2025-05-16 19:09:47 +0900

    feat: 필터버튼 컴포넌트 추가

---
+++ src/components/harviewer/HarFilterButton.tsx
@@ -0,0 +1,30 @@
+interface FilterButtonProps {
+  options: string[];
+  value: string;
+  onChange: (value: string) => void;
+}
+
+const HarFilterButton = ({ options, value, onChange }: FilterButtonProps) => {
+  return (
+    <div className="flex gap-2">
+      {options.map((option) => (
+        <button
+          key={option}
+          type="button"
+          className={`px-5 py-2 rounded transition
+            ${
+              value === option
+                ? "bg-gray-200 text-black font-semibold"
+                : "bg-white text-gray-500 border border-gray-200 hover:bg-gray-100"
+            }
+          `}
+          onClick={() => onChange(option)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default HarFilterButton;
