df02494a93347e5f3278ccb420dcb610835eefdc
JEONGHWANMUN <answjdghks95@gmail.com>
2025-05-20 16:32:52 +0900

    fix: box컴포넌트 children 프로퍼티 추가

--- src/components/common/Box.tsx
+++ src/components/common/Box.tsx
@@ -1,5 +1,5 @@
-const Box = () => {
-  return <div className="border-2 border-gray-300 p-8 rounded-xl text-center  bg-white"></div>;
+const Box = ({ children }: { children: React.ReactNode }) => {
+  return <div className="rounded-xl border border-gray-200 bg-gray-50 p-5 shadow-sm">{children}</div>;
 };
 
 export default Box;
