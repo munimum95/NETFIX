5f580ffe285137c1ad208c4b0cac487fc3dbd4f0
JEONGHWANMUN <answjdghks95@gmail.com>
2025-05-20 16:35:15 +0900

    feat: har파일관련 타입 추가

---
+++ src/types/har.ts
@@ -0,0 +1,53 @@
+export interface HarEntry {
+  startedDateTime: string;
+  time: number;
+  request: {
+    url: string;
+    method: string;
+    headers: { name: string; value: string }[];
+    postData?: {
+      mimeType: string;
+      text: string;
+    };
+  };
+  response: {
+    status: number;
+    statusText: string;
+    headers: { name: string; value: string }[];
+    content: {
+      size: number;
+      mimeType: string;
+      text?: string;
+    };
+  };
+  timings: {
+    wait: number;
+    receive: number;
+  };
+}
+
+export interface HarData {
+  log: {
+    entries: HarEntry[];
+  };
+}
+export interface ParsedHarEntry {
+  responseSize: number;
+  url: string;
+  status: number;
+  type: string;
+  size: number;
+  time: number;
+  method: string;
+  startedDateTime: string;
+  timings: {
+    wait: number;
+    receive: number;
+  };
+}
+
+export interface HarTableProps {
+  entries: ParsedHarEntry[];
+}
+
+export type FilterType = "All" | "XHR" | "JS" | "CSS" | "Img" | "Media" | "Other" | "Errors";
