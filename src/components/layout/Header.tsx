4d075fe6393d1b7577ab85a5fcd0160b625befbf
JEONGHWANMUN <answjdghks95@gmail.com>
2025-05-21 18:35:09 +0900

    style: 헤더 메인 색상 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -6,7 +6,7 @@ interface HeaderProps {
 const Header = ({ title, description }: HeaderProps) => {
   return (
     <header className="text-center">
-      <h1 className="text-3xl font-bold text-gray-800">{title}</h1>
+      <h1 className="text-4xl font-bold text-main">{title}</h1>
       <p className="text-gray-600">{description}</p>
     </header>
   );
